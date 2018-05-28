14339ab02b3e570adbdf46e17eb58d5a344764da
Unknown <nguyentrisinh0810@gmail.com>
2018-05-28 10:19:52 +0700

    Add Calendly addon into base.html

--- web/addons_plugin/cms_plugins.py
+++ web/addons_plugin/cms_plugins.py
@@ -44,3 +44,24 @@ class CalendlyAddonPlugin(CMSPluginBase):
         context['calendly_object'] = json.dumps(calendly_object)
 
         return context
+
+
+@plugin_pool.register_plugin
+class CalendlyMenuAddonPlugin(CMSPluginBase):
+    model = CalendlyPlugin
+    render_template = 'addons_plugin/plugin/calendly-addon-menu-component.html'
+    cache = True
+    form = CalendlyPluginForm
+
+    def render(self, context, instance, placeholder):
+        context = super(CalendlyMenuAddonPlugin, self).render(context, instance, placeholder)
+        instance = context['instance']
+
+        calendly_object = {
+            'calendly_embedded_url': instance.calendly_embedded_url,
+            'text_message': instance.text_message,
+            'color': instance.color
+        }
+        context['calendly_object'] = json.dumps(calendly_object)
+
+        return context
